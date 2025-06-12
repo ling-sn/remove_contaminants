f7bf2c357c18ba648c5f2fb23b32922bb5ff6e26
Sonia Ling <lingsn@umich.edu>
2025-06-12 13:56:57 -0400

    remove init of sanitize_header() in rm_contam()

--- rm_contam.py
+++ rm_contam.py
@@ -207,7 +207,6 @@ def rmcontam_pipeline(folder_path, output_path):
                     continue
             
             ## merge bam files, convert to bai, & remove files
-            samtools.sanitize_header()
             samtools.merge_bam(samtools_folder, subfolder)
 
 if __name__ == "__main__":
