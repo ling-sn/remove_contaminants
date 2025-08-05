69c04e1f602e3e455ca5d51afbd736d02c1811f9
Sonia <lingsn@umich.edu>
2025-08-05 16:19:40 -0400

    move samtools_folder variable outside of 'if bamfile'

--- rm_contam/rm_contam.py
+++ rm_contam/rm_contam.py
@@ -151,8 +151,9 @@ def rmcontam_pipeline(folder_name, bamfile):
             unmapped_folder = current_path/"filtered_processed_fastqs"/"removed_contam"/folder_name/f"{subfolder.name}"
             unmapped_folder.mkdir(exist_ok=True, parents=True)
 
+            
+            samtools_folder = mapped_folder/"samtools"
             if bamfile:
-                samtools_folder = mapped_folder/"samtools"
                 samtools_folder.mkdir(exist_ok=True, parents=True)
 
             for file in subfolder.glob("*.fastq.gz"): ## iterate through indiv. files in subfolder
