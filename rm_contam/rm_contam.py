6ab590bbf6265c127871c9f2dabe445408c8784d
Sonia <lingsn@umich.edu>
2025-08-05 14:25:14 -0400

    added boolean argument to control whether or not samfile is created

--- rm_contam/rm_contam.py
+++ rm_contam/rm_contam.py
@@ -9,22 +9,26 @@ class Bowtie2Aligner:
         self.r1_filename = None
         self.r2_filename = None
         
-    def single_reads(self, file, mapped_folder, unmapped_folder):
+    def single_reads(self, samfile, file, mapped_folder, unmapped_folder, samtools_folder):
         """
         Align single-end reads (merged/unpaired)
         """
         filename = file.name.split(".")[0]
         
         try:
-            # sam_output = samtools_folder/f"{filename}_mapped.sam"
+            sam_output = samtools_folder/f"{filename}_mapped.sam"
             contam_output = mapped_folder/f"{filename}_mapped.fastq.gz"
             rmcontam_output = unmapped_folder/f"{filename}_unmapped.fastq.gz"
+
             cmd = ["bowtie2", 
                     "-x", str(self.bowtie2_index), ## provides index that we created earlier
                     "-U", str(file), ## specifies single-read
+                    "-S", str(sam_output), ## .sam file of contam reads
                     "--un-gz", str(rmcontam_output), ## merged/unpaired reads that failed to align (mrna)
                     "--al-gz", str(contam_output)] ## merged/unpaired reads that aligned ≥1 times
-                    # "-S", str(sam_output), ## .sam file of contam reads
+            if samfile:
+                cmd.extend(["-S", str(sam_output)]) ## .sam file of contam reads
+                
             result = subprocess.run(cmd, 
                                     check = True, 
                                     capture_output = True, 
@@ -45,24 +49,27 @@ class Bowtie2Aligner:
             self.r1_filename = r1_file
             self.r2_filename = r1_file.with_name(r1_file.name.replace("_R1_", "_R2_"))
 
-    def paired_reads(self, file, mapped_folder, unmapped_folder):
+    def paired_reads(self, samfile, file, mapped_folder, unmapped_folder, samtools_folder):
         """
         Align paired-end reads (unmerged)
         """
         filename = file.name.split(".")[0]
 
         try:
-            # sam_output = samtools_folder/f"{filename}_mapped.sam"
+            sam_output = samtools_folder/f"{filename}_mapped.sam"
             contam_output = mapped_folder/f"{filename}_mapped.fastq.gz"
             rmcontam_output = unmapped_folder/f"{filename}_unmapped.fastq.gz"
             self.detect_reps(file.parent)
+
             cmd = ["bowtie2",
                     "-x", str(self.bowtie2_index),
                     "-1", str(self.r1_filename),
                     "-2", str(self.r2_filename),
                     "--un-conc-gz", str(rmcontam_output), ## unmerged reads that failed to align (mrna)
                     "--al-conc-gz", str(contam_output)] ## unmerged reads that aligned ≥1 times
-                    # "-S", str(sam_output) ## .sam file of contam reads
+            if samfile:
+                cmd.extend(["-S", str(sam_output)]) ## .sam file of contam reads
+
             result = subprocess.run(cmd, 
                                     check = True, 
                                     capture_output = True, 
@@ -128,7 +135,7 @@ class Bowtie2Aligner:
             traceback.print_exc()
             raise
 
-def rmcontam_pipeline(folder_name):
+def rmcontam_pipeline(folder_name, samfile):
     ## define input directory
     current_path = Path.cwd()
     input_dir = current_path/folder_name
@@ -144,34 +151,38 @@ def rmcontam_pipeline(folder_name):
             unmapped_folder = current_path/"filtered_processed_fastqs"/"removed_contam"/folder_name/f"{subfolder.name}"
             unmapped_folder.mkdir(exist_ok=True, parents=True)
 
-            # samtools_folder = processed_folder/"samtools"
-            # samtools_folder.mkdir(exist_ok=True, parents=True)
+            samtools_folder = mapped_folder/"samtools"
+            samtools_folder.mkdir(exist_ok=True, parents=True)
 
             for file in subfolder.glob("*.fastq.gz"): ## iterate through indiv. files in subfolder
                 try:
                     ## run bowtie2 alignment functions
                     if "_merged" in file.name or "_unpaired" in file.name:
-                        aligner.single_reads(file, mapped_folder, unmapped_folder)
+                        aligner.single_reads(samfile, file, mapped_folder, unmapped_folder, samtools_folder)
                     elif "_unmerged" in file.name:
-                        aligner.paired_reads(file, mapped_folder, unmapped_folder)
+                        aligner.paired_reads(samfile, file, mapped_folder, unmapped_folder, samtools_folder)
+                    else:
+                        continue
 
-                    ## run samtools function
-                    # aligner.convert_sam(samtools_folder, file)
-                    
+                    if samfile:
+                        ## run samtools function
+                        aligner.convert_sam(samtools_folder, file)
                 except Exception as e:
                     print(f"Failed to align {file.name} with bowtie2: {e}")
                     traceback.print_exc()
                     continue
             
-            ## merge bam files, convert to bai, & remove files
-            # aligner.merge_bam(samtools_folder, file)
+            if samfile:
+                ## merge bam files, convert to bai, & remove files
+                aligner.merge_bam(samtools_folder, file)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Run contaminant removal pipeline.")
     parser.add_argument("--folder_name", required = True, help = "Input processed_fastqs folder name")
+    parser.add_argument("-S", "--samfile", action = argparse.BooleanOptionalAction, default = False, help = "Create BAM output for mapped contaminants. The default is '--no-samfile', but you can enable with '--samfile'.")
 
     args = parser.parse_args()
 
     print("Starting contaminant removal pipeline...")
-    rmcontam_pipeline(args.folder_name)
+    rmcontam_pipeline(args.folder_name, args.samfile)
     print("Pipeline finished.")
